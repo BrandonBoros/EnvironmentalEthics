3e6bbc83d2050074ffa353c45a0d0e08f8052d7d
BrandonBoros <2377215@johnabbottcollege.net>
2024-12-01 19:04:13 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Show the current slide
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function changeSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    slideIndex += n;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Show the current slide
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Initialize the slideshow
+showSlides();
